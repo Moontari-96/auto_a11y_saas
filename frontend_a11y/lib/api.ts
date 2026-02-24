d2fcf9dd96ade7bea01d57d7ac426e7896529d42
moontari <shaaa6256@gmail.com>
2026-02-24 17:50:22 +0900

    주소 변경

--- frontend_a11y/lib/api.ts
+++ frontend_a11y/lib/api.ts
@@ -1,7 +1,12 @@
 import axios from 'axios'
 
+// 환경 변수가 있으면 EC2 주소를 쓰고, 없으면 로컬 주소를 사용합니다.
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE
+    ? `${process.env.NEXT_PUBLIC_API_BASE}/a11y`
+    : 'http://localhost:3001/a11y'
+
 export const api = axios.create({
-    baseURL: 'http://localhost:3001/a11y',
+    baseURL: BASE_URL,
     headers: {
         'Content-Type': 'application/json',
     },
